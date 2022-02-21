b9fc81a07cf63d1e4eb31206b5b126d850fca6cb
Isaiah Swanson <isaiahpaul.swanson@calbaptist.edu>
2022-02-21 14:48:43 -0800

    initial commit

---
+++ functionality.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", domLoaded);
+function domLoaded () {
+
+    document.querySelector("#custom-submit-button").addEventListener("click", submitEventHandler);
+}
+
+submitEventHandler(); {
+    fileupload.on('change', function(){}); // Gets user input file
+}
